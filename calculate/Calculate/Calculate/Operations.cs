80320c18f06c8bfd7da366e41d05f945a415db4d
AyxanNabatov <107032159+AyxanNabatov@users.noreply.github.com>
2022-07-04 06:50:21 +0400

    Add files via upload

---
+++ calculate/Calculate/Calculate/Operations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculate
+{
+    public class Operations : ICalculate
+    {
+        public void Calculation(int num1, int num2, string operation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Calculator(int num1, int num2, string Operation)
+        {
+            if (Operation == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+
+            if (Operation == "-")
+            {
+                Console.WriteLine(num1 - num1);
+            }
+
+            if (Operation == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+
+            if (Operation == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+        }
+    }
+}
